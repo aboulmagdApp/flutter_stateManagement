34cd800a2a69ea91d1a79d886bbe98fc25f09595
aboulmagd <aboulmagd@live.com>
2024-04-25 10:42:26 +0300

    change to use provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,104 +1,76 @@
 import 'package:flutter/material.dart';
 import 'package:mngmt_flutter/utils.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (_) => AppState(),
+    child: const App(),
+  ));
 }
 
-class MyApp extends StatefulWidget {
-  const MyApp({super.key});
+class App extends StatelessWidget {
+  const App({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'viewer',
+      home: GalleryPage(title: 'viewer'),
+    );
+  }
 }
 
-class _MyAppState extends State<MyApp> {
+class AppState extends ChangeNotifier {
   bool isTagging = false;
   List<PhotoState> photoStates =
       List.of(Utils.urls.map((url) => PhotoState(url)));
   Set<String> tags = Utils.tags;
 
   void selectTag(String tag) {
-    setState(() {
-      if (isTagging) {
-        if (tag != "all") {
-          for (var element in photoStates) {
-            if (element.selected) {
-              element.tags.add(tag);
-            }
-          }
-          toggleTagging(null);
-        }
-      } else {
+    if (isTagging) {
+      if (tag != "all") {
         for (var element in photoStates) {
-          element.display = tag == "all" ? true : element.tags.contains(tag);
+          if (element.selected) {
+            element.tags.add(tag);
+          }
         }
       }
-    });
+      toggleTagging(null);
+    } else {
+      for (var element in photoStates) {
+        element.display = tag == "all" ? true : element.tags.contains(tag);
+      }
+    }
+    notifyListeners();
   }
 
   void toggleTagging(String? url) {
-    setState(() {
-      isTagging = !isTagging;
-      for (var element in photoStates) {
-        if (isTagging && element.url == url && url != null) {
-          element.selected = true;
-        } else {
-          element.selected = false;
-        }
+    isTagging = !isTagging;
+    for (var element in photoStates) {
+      if (isTagging && element.url == url && url != null) {
+        element.selected = true;
+      } else {
+        element.selected = false;
       }
-    });
+    }
+    notifyListeners();
   }
 
   void onPhotoSelect(String url, bool selected) {
-    setState(() {
-      for (var element in photoStates) {
-        if (element.url == url) {
-          element.selected = selected;
-        }
+    for (var element in photoStates) {
+      if (element.url == url) {
+        element.selected = selected;
       }
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: GalleryPage(
-        title: "Image Gallery",
-        photoStates: photoStates,
-        tagging: isTagging,
-        tags: tags,
-        selectTag: selectTag,
-        toggleTagging: toggleTagging,
-        onPhotoSelect: onPhotoSelect,
-      ),
-    );
+    }
+    notifyListeners();
   }
 }
 
 class GalleryPage extends StatelessWidget {
   final String title;
-  final List<PhotoState> photoStates;
-  final bool tagging;
-  final Set<String> tags;
 
-  final Function toggleTagging;
-  final Function onPhotoSelect;
-  final Function selectTag;
-
-  const GalleryPage(
-      {super.key,
-      required this.title,
-      required this.photoStates,
-      required this.tagging,
-      required this.toggleTagging,
-      required this.onPhotoSelect,
-      required this.tags,
-      required this.selectTag});
+  const GalleryPage({super.key, required this.title, g});
 
   @override
   Widget build(BuildContext context) {
@@ -108,24 +80,21 @@ class GalleryPage extends StatelessWidget {
           //padding: const EdgeInsets.all(8.0),
           primary: false,
           crossAxisCount: 2, // Add padding around the GridView
-          children: photoStates
+          children: context
+              .watch<AppState>()
+              .photoStates
               .where((ps) => ps.display)
               .map((ps) => Photo(
                     state: ps,
-                    selectable: tagging,
-                    onLongPress: toggleTagging,
-                    onSelect: onPhotoSelect,
                   ))
               .toList()),
       drawer: Drawer(
         child: ListView(
-          children: List.of((tags.map((t) => ListTile(
+          children: List.of((context.watch<AppState>().tags.map((t) => ListTile(
                 title: Text(t),
                 onTap: () {
-                  selectTag(t);
+                  context.read<AppState>().selectTag(t);
                   Navigator.of(context).pop();
-                  print('drawer clicked');
-                  print(photoStates);
                 },
               )))),
         ),
@@ -136,24 +105,17 @@ class GalleryPage extends StatelessWidget {
 
 class Photo extends StatelessWidget {
   final PhotoState state;
-  final bool selectable;
-
-  final Function onLongPress;
-  final Function onSelect;
 
   const Photo({
     super.key,
     required this.state,
-    required this.selectable,
-    required this.onLongPress,
-    required this.onSelect,
   });
 
   @override
   Widget build(BuildContext context) {
     List<Widget> children = [
       GestureDetector(
-        onLongPress: () => onLongPress(state.url),
+        onLongPress: () => context.read<AppState>().toggleTagging(state.url),
         child: Image.network(
           state.url,
           fit: BoxFit.cover,
@@ -161,7 +123,7 @@ class Photo extends StatelessWidget {
       )
     ];
 
-    if (selectable) {
+    if (context.watch<AppState>().isTagging) {
       children.add(Positioned(
           left: 20,
           top: 0,
@@ -169,10 +131,11 @@ class Photo extends StatelessWidget {
             data: Theme.of(context)
                 .copyWith(unselectedWidgetColor: Colors.grey[200]),
             child: Checkbox(
-              onChanged: ((value) => onSelect(
-                    state.url,
-                    value,
-                  )),
+              onChanged: (value) {
+                context
+                    .read<AppState>()
+                    .onPhotoSelect(state.url, value ?? false);
+              },
               value: state.selected,
               activeColor: Colors.white,
               checkColor: Colors.black,
